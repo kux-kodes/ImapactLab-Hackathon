fc1882d3a521523689dfd27342b35edb73597e19
Jacinto Celestino Tchayevala <celestinojcj@gmail.com>
2025-03-25 23:38:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path'); // Import path module
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public')); // Serve static files from the public directory
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Serve index.html as the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/fetchJobs', async (req, res) => {
     const { count, scrapeCompany, urls } = req.query;
@@ -11,18 +18,14 @@ app.get('/fetchJobs', async (req, res) => {
     try {
         const response = await axios.get(urls[0], {
             headers: {
-                'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
             }
         });
 
-        
-        const jobListings = response.data; 
-
-        
         res.json({
             count: count,
             scrapeCompany: scrapeCompany,
-            jobs: jobListings 
+            jobs: response.data
         });
     } catch (error) {
         res.status(500).json({ error: 'Error fetching job listings: ' + error.message });
